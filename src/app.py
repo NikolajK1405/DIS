29f3c477720bbc238b6fc6641a53b71cd310e550
VTcommander <emil.vedel.thage@gmail.com>
2024-06-07 15:20:42 +0200

    First try at a homepage

--- src/app.py
+++ src/app.py
@@ -4,22 +4,41 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 import glob
+import re
+
+from psycopg2.sql import NULL
 
 app = Flask(__name__)
 
-db = "dbname='***' user='***' host='***' password='***'"
+db = "dbname='***' user='***' host='localhost' password='***'"
 
 conn = psycopg2.connect(db)
 cur = conn.cursor()
 
 bcrypt = Bcrypt(app)
-     
+
+# Passwords must contain the word kebab, an uppercase letter, and no special chars
+re_password = r'(?=.*(kebab|Kebab).*)(?=.*[A-Z].*)[a-zA-Z0-9]*'
+
 @app.route('/',  methods=['POST', 'GET'])
-def home():
+def start():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return 'du er logget ind!'
+        return redirect(url_for("home"))
+
+@app.route('/home',  methods=['POST', 'GET'])
+def home():
+    username = None
+    kebabnum = None
+    if session.get('logged_in'):
+        uid = session.get('uid')[0]
+        find = " SELECT username, kebabnum from users where uid = %s"
+        cur.execute(find, (uid,))
+        user = cur.fetchone()
+        username = user[0]
+        kebabnum = user[1]
+    return render_template("home.html", username=username, kebabnum = kebabnum)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -31,33 +50,38 @@ def login():
 
     cur.execute(find, (username, password))
 
-    LoggedIn = len(cur.fetchall()) != 0
+    user = cur.fetchall()
+    LoggedIn = len(user) != 0
 
     if LoggedIn:
         session['logged_in'] = True
-        session['username'] = username
+        session['uid'] = user[0]
     else:
         flash('wrong password!')
-    return redirect(url_for("home"))
+    return redirect(url_for("start"))
 
 @app.route('/cAccount', methods=['POST', 'GET'])
 def cAccount():
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
-        
+        password = request.form['password']        
+
         find = "select * from users where username = %s"
         cur.execute(find, (username,))
-        unique = cur.fetchall()
-        
+        unique = cur.fetchall()        
+
         if len(unique) == 0:
+            if not re.match(re_password, password):
+                flash('''Invalid password, your password must contain an upper case letter and the word kebab. 
+                      No special characters are allowed''')
+                return redirect(url_for("cAccount"))
             cur.execute("select * from users")
             count = len(cur.fetchall())
             insert = "INSERT INTO users(uid, username, password, kebabnum) VALUES (%s, %s, %s, %s)"
             cur.execute(insert, (count+1, username, password, 0))
             conn.commit()
             flash('Account creation succesful')
-            return redirect(url_for("home"))
+            return redirect(url_for("start"))
         else: 
             flash('Username already exists!')
     return render_template("cAccount.html")
