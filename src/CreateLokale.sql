9d3d8a58cb19c01ed08ef7fcbd36e40871c31ee9
NikolajK1405 <Nikolajkrarup@hotmail.com>
2024-06-09 13:28:55 +0200

    den lokale vists

--- src/CreateLokale.sql
+++ src/CreateLokale.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS lokale; -- hjælper på test, men slet senere
 
-CREATE TABLE IF NOT EXISTS lokale( uid char(3), kid char(3) NOT NULL,
+CREATE TABLE IF NOT EXISTS lokale( uid char(3), kid char(3) NOT NULL, visits int,
     PRIMARY KEY (uid));
